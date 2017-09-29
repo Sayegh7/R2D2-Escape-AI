21ddc7c78170977e3004f0e9bd54c95c4b4a8857
loay <loayelzobeidy@gmail.com>
2017-09-29 22:15:50 +0200

    Queueing function for BFS and DFS Done

--- GenericSearch.py
+++ GenericSearch.py
@@ -4,25 +4,27 @@ import queue
 import Visualizer as v
 import numpy as np
 def Search(problem, strategy, visualize, gui):
+    global nodes
     global visitedMatrices
     global rockCount
     rockCount = 0
     visitedMatrices = []
-    if strategy == "BFS":
-        nodes = queue.LifoQueue()
+    # if strategy == "BFS":
+    nodes = queue.Queue()
     (x,y,grid,max) = problem.initalState
     rockCount = np.count_nonzero(grid==1)
 
     rootNode = SearchNode.createNode((x, y, grid, max), 0, 0, None, None)
     nodes.put(rootNode)
     while nodes.empty() == False:
+        newNodes =queue.Queue()
         flag = False
         node = nodes.get()
         v.renderGrid(node.state[2], 5)
         gui.refresh()
         gridArray = node.state[2]
         if np.count_nonzero(node.state[2]==1) != rockCount:
-            nodes = queue.LifoQueue()
+            nodes = queue.Queue()
             rockCount = np.count_nonzero(node.state[2]==1)
             nodes.put(node)
             continue;
@@ -35,8 +37,19 @@ def Search(problem, strategy, visualize, gui):
            for matrix in visitedMatrices:
                if np.array_equal(matrix,gridArray):
                         # print (matrix)
-                        flag =True    
-           if flag == False: 
-                nodes.put(node)
+                        flag =True
+           if flag == False:
+                newNodes.put(node)
         visitedMatrices.append(gridArray)
-    return 
+        Queueingfucntion(newNodes,strategy)
+    return
+def Queueingfucntion(newNodes,strategy):
+    global nodes
+    if strategy == "BFS":
+        while not newNodes.empty():
+            nodes.put(newNodes.get())
+    if strategy == "DFS":
+        while not nodes.empty():
+            newNodes.put(nodes.get())
+        while not newNodes.empty():
+            nodes.put(newNodes.get())

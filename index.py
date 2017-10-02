7dcdc44abfd4073bc00fa09b99d110ba33e3c268
Ahmed El Sayegh <ahmedelsayegh7@gmail.com>
2017-10-03 00:09:12 +0200

    Abstracted State and Transition Function

--- index.py
+++ index.py
@@ -1,9 +1,8 @@
 import SearchProblem
 import GridGenerator
 import SearchNode
+from State import State
 import GenericSearch
-
-
 
 def run(gui):
         grid = GridGenerator.GenGrid()
@@ -14,22 +13,25 @@ def run(gui):
 
         operators = ['Up', 'Down', 'Left', 'Right']
         def goalTestFunction(state):
-            print(state.max)
             return (state.max == 0)
 
         problem = SearchProblem.createSearchProblem(initialState, operators, goalTestFunction)
 
-        result_node = GenericSearch.Search(problem, "BFS", True, gui);
+        result_node = GenericSearch.Search(problem, "BFS", True, gui=gui);
 
         if result_node != None:
             path = []
+            print_path = []
             path.append(result_node.operator)
             parent = result_node.parent
             while parent != None:
                 if parent.operator != None:
                     path.append(parent.operator)
                 parent = parent.parent
-            print("Solution", path)
+            print("Solution:")
+            for value in reversed(path):
+                print_path.append(value)
+            print (print_path)
         else:
             print("Found no solution")
 # run(None)

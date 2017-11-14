d57758edb0cbbffea6a840e5d78ebf8d8c370eb7
loay elzobaidy <loayelzobeidy@gmail.com>
2017-11-14 18:34:11 +0200

    knowledebase updated

--- knowledgebase.pl
+++ knowledgebase.pl
@@ -1,101 +1,40 @@
 % 
-rock(5,8,s).
 pad(3,4,s).
-rock(6,5,s).
 pad(5,3,s).
-rock(1,4,s).
 pad(3,8,s).
-rock(4,7,s).
 pad(8,7,s).
+rock(5,8,s).
+rock(6,5,s).
+rock(1,4,s).
+rock(4,7,s).
 immovable(5,2,s).
 immovable(8,8,s).
 teleporter(4,1,s).
-r2d2(8,2,s).
-cell(0,0,s).
-cell(0,1,s).
-cell(0,2,s).
-cell(0,3,s).
-cell(0,4,s).
-cell(0,5,s).
-cell(0,6,s).
-cell(0,7,s).
-cell(0,8,s).
-cell(0,9,s).
-cell(1,0,s).
-cell(1,1,s).
-cell(1,2,s).
-cell(1,3,s).
-cell(1,5,s).
-cell(1,6,s).
-cell(1,7,s).
-cell(1,8,s).
-cell(1,9,s).
-cell(2,0,s).
-cell(2,1,s).
-cell(2,2,s).
-cell(2,3,s).
-cell(2,4,s).
-cell(2,5,s).
-cell(2,6,s).
-cell(2,7,s).
-cell(2,8,s).
-cell(2,9,s).
-cell(3,0,s).
-cell(3,1,s).
-cell(3,2,s).
-cell(3,3,s).
-cell(3,5,s).
-cell(3,6,s).
-cell(3,7,s).
-cell(3,9,s).
-cell(4,0,s).
-cell(4,2,s).
-cell(4,3,s).
-cell(4,4,s).
-cell(4,5,s).
-cell(4,6,s).
-cell(4,8,s).
-cell(4,9,s).
-cell(5,0,s).
-cell(5,1,s).
-cell(5,4,s).
-cell(5,5,s).
-cell(5,6,s).
-cell(5,7,s).
-cell(5,9,s).
-cell(6,0,s).
-cell(6,1,s).
-cell(6,2,s).
-cell(6,3,s).
-cell(6,4,s).
-cell(6,6,s).
-cell(6,7,s).
-cell(6,8,s).
-cell(6,9,s).
-cell(7,0,s).
-cell(7,1,s).
-cell(7,2,s).
-cell(7,3,s).
-cell(7,4,s).
-cell(7,5,s).
-cell(7,6,s).
-cell(7,7,s).
-cell(7,8,s).
-cell(7,9,s).
-cell(8,0,s).
-cell(8,1,s).
-cell(8,3,s).
-cell(8,4,s).
-cell(8,5,s).
-cell(8,6,s).
-cell(8,9,s).
-cell(9,0,s).
-cell(9,1,s).
-cell(9,2,s).
-cell(9,3,s).
-cell(9,4,s).
-cell(9,5,s).
-cell(9,6,s).
-cell(9,7,s).
-cell(9,8,s).
-cell(9,9,s).
+robot(8,7,s).
+
+
+
+
+robot(X,Y,result(A,S)):-
+	(
+		(
+		(A=left,H is X+1, robot(H,Y,S));
+		(A=right,H is X-1, robot(H,Y,S));
+		(A=up,V is Y-1, robot(X,V,S));
+		(A=down,V is Y+1, robot(X,V,S))
+		)
+	);
+(
+	(
+       (A=left,immovable(X-1,Y,S),H is X+1,robot(H,Y,S));
+       (A=right,immovable(X+1,Y,S),H is X-1 ,robot(H,Y,S));
+       (A=up,immovable(X,Y-1,S),V is Y-1 ,robot(X,V,S));
+       (A=down,immovable(X,Y+1,S),V is Y+1 ,robot(X,V,S))
+		)
+	).
+
+generate(X,Y,S,N):-
+ ((call_with_depth_limit(robot(X,Y,S),N,Z)), \+Z=depth_limit_exceeded)
+ ;(( call_with_depth_limit(robot(X,Y,S),N,depth_limit_exceeded)), N1 is N+1,generate(X,Y,S,N1)).
+
+%
